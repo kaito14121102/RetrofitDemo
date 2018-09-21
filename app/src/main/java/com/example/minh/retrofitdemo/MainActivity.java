6537d609fdd86b6cda0c4c426f0b2dfd4e8f8787
kaito14121102 <luutuanminh.cntt@gmail.com>
2018-09-21 17:40:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/minh/retrofitdemo/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.minh.retrofitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.minh.retrofitdemo.data.model.Item;
+import com.example.minh.retrofitdemo.data.model.SOAnswersResponse;
+import com.example.minh.retrofitdemo.data.remote.ApiUltils;
+import com.example.minh.retrofitdemo.data.remote.SOService;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private AnswersAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+    private SOService mService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mService = ApiUltils.getSOService();
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycle_view);
+        mAdapter = new AnswersAdapter(this, new ArrayList<Item>(0), new AnswersAdapter.PostItemListener() {
+
+            @Override
+            public void onPostClick(long id) {
+                Toast.makeText(MainActivity.this, "Post id is" + id, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        mRecyclerView.addItemDecoration(itemDecoration);
+        loadAnswers();
+    }
+
+    public void loadAnswers() {
+        mService.getAnswers().enqueue(new Callback<SOAnswersResponse>() {
+            @Override
+            public void onResponse(Call<SOAnswersResponse> call, Response<SOAnswersResponse> response) {
+                if(response.isSuccessful()) {
+                    mAdapter.updateAnswers(response.body().getItems());
+                }else {
+                    int statusCode = response.code();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<SOAnswersResponse> call, Throwable t) {
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/minh/retrofitdemo/data/remote/RetrofitClient.java
@@ -0,0 +1,46 @@
+package com.example.minh.retrofitdemo.data.remote;
+
+import android.text.TextUtils;
+
+import com.example.minh.retrofitdemo.Constant;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Credentials;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Minh on 9/20/2018.
+ */
+
+public class RetrofitClient {
+    public static Retrofit getClient(String baseUrl) {
+        System.setProperty("http.keepAlive", "false");
+        final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .readTimeout(60, TimeUnit.SECONDS)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .build();
+        Retrofit retrofit;
+
+        if (baseUrl != null && !TextUtils.isEmpty(baseUrl)) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient)
+                    .build();
+        } else {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(Constant.HOME_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
